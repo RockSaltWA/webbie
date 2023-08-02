dabc0ad26a3f2c508031767a65d498c693bb04dc
Echo <jack.doughty11@gmail.com>
2023-08-01 22:19:35 -0700

    test v1

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -4,12 +4,12 @@ import {FC, memo} from 'react';
 
 import {contact, SectionId} from '../../../data/data';
 import {ContactType, ContactValue} from '../../../data/dataDef';
+import DiscordIcon from '../../Icon/DiscordIcon';
 import FacebookIcon from '../../Icon/FacebookIcon';
 import GithubIcon from '../../Icon/GithubIcon';
 import InstagramIcon from '../../Icon/InstagramIcon';
 import LinkedInIcon from '../../Icon/LinkedInIcon';
 import TwitterIcon from '../../Icon/TwitterIcon';
-import DiscordIcon from '../../Icon/DiscordIcon';
 import Section from '../../Layout/Section';
 import ContactForm from './ContactForm';
 
@@ -22,7 +22,7 @@ const ContactValueMap: Record<ContactType, ContactValue> = {
   [ContactType.Facebook]: {Icon: FacebookIcon, srLabel: 'Facebook'},
   [ContactType.Twitter]: {Icon: TwitterIcon, srLabel: 'Twitter'},
   [ContactType.Instagram]: {Icon: InstagramIcon, srLabel: 'Instagram'},
-  [ContactType.Discord]: { Icon: DiscordIcon, srLabel: 'Discord' },
+  [ContactType.Discord]: {Icon: DiscordIcon, srLabel: 'Discord'},
 };
 
 const Contact: FC = memo(() => {
