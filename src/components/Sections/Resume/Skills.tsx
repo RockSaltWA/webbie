2ece15231e1eafa6bb71f051adeb6cfcab1852aa
Echo <jack.doughty11@gmail.com>
2023-08-03 12:51:49 -0700

    ok take 2

--- src/components/Sections/Resume/Skills.tsx
+++ src/components/Sections/Resume/Skills.tsx
@@ -1,8 +1,9 @@
-import { FC, memo, PropsWithChildren, useMemo } from 'react';
-import { Skill as SkillType, SkillGroup as SkillGroupType } from '../../../data/dataDef';
+import {FC, memo, PropsWithChildren, useMemo} from 'react';
 
-export const SkillGroup: FC<PropsWithChildren<{ skillGroup: SkillGroupType }>> = memo(({ skillGroup }) => {
-    const { name, skills } = skillGroup;
+import {Skill as SkillType, SkillGroup as SkillGroupType} from '../../../data/dataDef';
+
+export const SkillGroup: FC<PropsWithChildren<{ skillGroup: SkillGroupType }>> = memo(({skillGroup}) => {
+    const {name, skills} = skillGroup;
     return (
         <div className="flex flex-col">
             <span className="text-center text-lg font-bold">{name}</span>
@@ -17,12 +18,12 @@ export const SkillGroup: FC<PropsWithChildren<{ skillGroup: SkillGroupType }>> =
 
 SkillGroup.displayName = 'SkillGroup';
 
-export const Skill: FC<{ skill: SkillType }> = memo(({ skill }) => {
-    const { name, level, max = 10 } = skill;
+export const Skill: FC<{ skill: SkillType }> = memo(({skill}) => {
+    const {name, level, max = 10} = skill;
     const percentage = useMemo(() => Math.round((level / max) * 100), [level, max]);
 
     const tickMarks = useMemo(() => {
-        let ticks = [];
+        const ticks = [];
         for (let i = 0; i <= max; i++) {
             ticks.push(i);
         }
@@ -33,11 +34,11 @@ export const Skill: FC<{ skill: SkillType }> = memo(({ skill }) => {
         <div className="flex flex-col">
             <span className="ml-2 text-sm font-medium">{name}</span>
             <div className="h-5 w-full overflow-hidden rounded-full bg-neutral-300">
-                <div className="h-full rounded-full bg-green-400" style={{ width: `${percentage}%` }} />
+                <div className="h-full rounded-full bg-green-400" style={{width: `${percentage}%`}} />
             </div>
             <div className="flex justify-between mt-1">
                 {tickMarks.map((tick, index) => (
-                    <span key={index} className="text-xs">{tick}</span>
+                    <span className="text-xs" key={index}>{tick}</span>
                 ))}
             </div>
         </div>
